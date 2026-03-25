ba189246020196429f05dd853eb3a257a2a5d3bb
Bianca Franco de Souza <bianca-franco1@hotmail.com>
2026-03-25 17:50:53 -0300

    fix: renomeação das variáveis para padronização em inglês

--- main.py
+++ main.py
@@ -5,40 +5,41 @@
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List, Union
 
-caracteres_especiais = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+']
+special_characters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+']
 app = FastAPI()
 
 class PasswordRequest(BaseModel):
-    senha:str
+    Password:str
 
 
-def regras_senha(senha:str) -> bool:
-    if len(senha) < 9:
+def password_rules(Password:str) -> bool:
+    if len(Password) < 9:
         print('A senha é muito curta')
         return False
-    if not any(char.isdigit() for char in senha):
+    if not any(char.isdigit() for char in Password):
         print('senha precisa conter pelo menos 1 número')
         return False
-    if not any(char.islower() for char in senha):
+    if not any(char.islower() for char in Password):
         print('senha precisa conter pelo menos 1 letra minuscula')
         return False
-    if not any(char.isupper() for char in senha):
+    if not any(char.isupper() for char in Password):
         print('senha precisa conter pelo menos 1 letra maiúscula')
         return False
-    if not any(char in list(caracteres_especiais) for char in senha):
+    if not any(char in list(special_characters) for char in Password):
         print('senha precisa conter pelo menos 1 caractere especial')
         return False
-    if len(set(senha)) != len (senha):
+    if len(set(Password)) != len (Password):
         print('senha não pode conter caracteres duplicados')
         return False
-    if " " in senha:
+    if " " in Password:
         print('senha não pode conter espaço')
         return False
     print('senha ok')
     return True
 
 @app.post("/user/validate")
-async def valida_senha(request: PasswordRequest):
-    return {"senha": request.senha}
+async def validation_pss(request: PasswordRequest):
+    return {"Password": request.Password}
 
